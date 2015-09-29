46a51e96e858a3a7c740c9e87900af35a7086d27
Gabriel Somoza <gabriel@somoza.me>
2015-09-29 09:53:14 +0200

    Add and configure sphpdox to automatically build reference

--- docs/bin/build-docs.sh
+++ docs/bin/build-docs.sh
@@ -3,5 +3,5 @@ EXECPATH=`dirname $0`
 cd $EXECPATH
 cd ..
 
-rm build -Rf
+#rm -rf build
 sphinx-build -b html en build/html
---
+++ docs/bin/generate-reference.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+EXECPATH=`dirname $0`
+cd $EXECPATH
+cd ..
+
+rm -rf en/ref/Baleen
+mkdir -p en/ref/Baleen
+../vendor/bin/sphpdox process -o en/ref Baleen\\Migrations ../lib
+find en/ref -name "*.rst" -exec bash -c 'mv "$1" "$(sed "s/\.rst$/.txt/" <<< "$1")"' - '{}' \;
